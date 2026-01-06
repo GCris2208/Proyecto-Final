3238721421c7a47c095d482d376e6614b5a8e587
Calo <eduyeli958@gmail.com>
2026-01-06 13:41:04 -0400

    Update config.js
    Gurren lagan lo mas duro del sistema

--- Proyecto/js/config.js
+++ Proyecto/js/config.js
@@ -1,4 +1,5 @@
 const VESTIA_CONFIG = {
-    API_KEY: "TU_API_KEY_AQUI", // Pega tu clave real aquí
+    API_KEY: "AIzaSyBDsimakhu0l0Gan2EAmYWGL5zWehO8rVE", // Pega tu clave real aquí
     MODEL_NAME: "gemini-2.5-flash"
+
 };
