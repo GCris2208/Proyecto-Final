7b5165ad4eae18f487eec24de98b6ab739964c68
GCris <cristian.barrios06@gmail.com>
2026-01-14 10:22:42 -0400

    Commit del programa sin la API_Key para repo publico

--- Proyecto/js/config.js
+++ Proyecto/js/config.js
@@ -1,5 +1,5 @@
-const VESTIA_CONFIG = {
-    API_KEY: "AIzaSyDdyvo8jIje109la8MLGGc6o1D2jD9g7IY", 
+const VESTIA_CONFIG = { 
+    API_KEY: "", 
     MODEL_NAME: "gemini-2.5-flash",
     API_URL: "https://dummyjson.com/products",
     CATEGORIES: [
