c2b3eef8024f262048564f79f94920db0802abe2
GCris <cristian.barrios06@gmail.com>
2026-01-14 10:51:01 -0400

    Commit de prueba cambiando orden de archivos, posibles errores

---
+++ js/config.js
@@ -0,0 +1,14 @@
+const VESTIA_CONFIG = { 
+    API_KEY: "", 
+    MODEL_NAME: "gemini-2.5-flash",
+    API_URL: "https://dummyjson.com/products",
+    CATEGORIES: [
+        "womens-dresses", 
+        "womens-shoes", 
+        "mens-shirts", 
+        "mens-shoes", 
+        "tops",
+        "sunglasses",
+        "womens-bags"
+    ]
+};
